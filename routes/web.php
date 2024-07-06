05601bfd86bffe58678ef3f45dd6833cac785dc4
Plan-B-Arko <freelancerorko@gmail.com>
2024-07-06 14:20:34 +0600

    admin template add and admin logout done

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,7 @@ require __DIR__.'/auth.php';
 Route::middleware(['auth','roles:admin'])->group(function(){
 
 Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
 
 });// admin groupp middleware
 
