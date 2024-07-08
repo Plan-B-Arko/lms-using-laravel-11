4cf7a3cdf5159a34834f2f086b4b209cce97d76d
Plan-B-Arko <freelancerorko@gmail.com>
2024-07-08 13:14:00 +0600

    image on load by javascript after file selec done

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,10 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
-
 
 class AdminController extends Controller
 {
@@ -29,4 +28,12 @@ class AdminController extends Controller
     public function AdminLogin(){
         return view('admin.admin_login');
     }
+
+    public function AdminProfile(){
+
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+        return view('admin.admin_profile_view', compact('profileData'));
+    }
+
 }
